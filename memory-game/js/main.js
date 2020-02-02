35474025f8aade2692b6619132b79130836bdd34
Max Meacham <maxmeacham@Maxs-MacBook-Pro.local>
2020-02-02 02:29:18 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,25 @@
-console.log("Up and running!");
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
-console.log("User flipped " + cardOne);
+
+const cards = ["queen", "queen", "king", "king"].length;
+
+const cardsInPlay = [];
+
+var cardOne = ["queen"];
+
+cardsInPlay.push('cardOne');
+
+console.log("User flipped queen")
+
+var cardTwo = ["king"];
+
+cardsInPlay.push('cardTwo');
+
+console.log("User flipped king");
+
+if (cardsInPlay === 2) {
+  alert("You found a match!");
+}
+// Could not get the else function to work.
+if (cardsInPlay !== 2) {
+  alert("Sorry try again!");
+}
